7beb46bed73c1b07790f5b271e07340749f21f46
yeshwanthkonk <37962427+yeshwanthkonk@users.noreply.github.com>
2020-07-04 18:44:14 +0530

    Update 7.js

--- 7.js
+++ 7.js
@@ -1,1 +1,37 @@
 
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+var userInput = []
+inp.on("line", (data) => {
+  userInput.push(data)
+});
+
+inp.on('close', () => {
+    if(userInput[0]<1 || userInput[0]>12){
+        console.log("Error")
+    }
+    else if(userInput[0] > 7){
+        if(userInput[0]%2 === 0){
+            console.log(31)
+        }
+        else{
+            console.log(30)
+        }
+    }
+    else{
+        if(userInput[0] == 2){
+            console.log(28)
+        }
+        else if(userInput[0]%2 === 0){
+            console.log(30)
+        }
+        else{
+            console.log(31)
+        }
+    }
+})
