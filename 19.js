12d8a4c7324980f3c8b8bd155cdc48ad86866ccb
yeshwanthkonk <37962427+yeshwanthkonk@users.noreply.github.com>
2020-07-05 12:53:54 +0530

    Create 19.js
    Successfully Executed

---
+++ 19.js
@@ -0,0 +1,16 @@
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+var ui = [];
+
+inp.on("line", (data) => {
+  ui.push(data);
+});
+
+inp.on('close', () => {
+    console.log( ( ( (+ui[0])*9)/5)+32)
+})
